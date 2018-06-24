73c2720fc541f7977fcd5e2b033ba279447eecd1
michalsosnowski <mihv.sosna@gmail.com>
2018-06-24 11:54:58 +0200

    Add bubble sort to hrManager

--- hr-module/src/main/java/pl/sdacademy/hr/HrMenager.java
+++ hr-module/src/main/java/pl/sdacademy/hr/HrMenager.java
@@ -31,13 +31,26 @@ class HrMenager {
 	}
 
 	public List<Employee> searchByPhrase(String phrase) {
-		return allEmployees.stream().filter(employee ->
-			employee.matches(phrase))
-			.collect(Collectors.toList());
+		return allEmployees.stream().filter(employee -> employee.matches(phrase)).collect(Collectors.toList());
 	}
 
 	public List<Employee> sortByFirstName() {
-		return allEmployees.stream().sorted().collect
-			(Collectors.toList());
+		return allEmployees.stream().sorted().collect(Collectors.toList());
+	}
+
+	public List<Employee> sortByFirstNameWithBubble() {
+		for (int j = 0; j < allEmployees.size() - 1; j++) {
+			for (int i = 0; i < allEmployees.size()-j - 1; i++) {
+
+				if (allEmployees.get(i).getFirstName().compareTo(allEmployees.get(i + 1).getFirstName
+					()) < 0) {
+					Employee temp = allEmployees.get(i);
+					allEmployees.set(i, allEmployees.get(i + 1));
+					allEmployees.set(i + 1, temp);
+				}
+			}
+		}
+		return allEmployees;
+
 	}
 }
--- hr-module/src/test/java/pl/sdacademy/hr/HrMenagerTest.java
+++ hr-module/src/test/java/pl/sdacademy/hr/HrMenagerTest.java
@@ -9,22 +9,23 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 
 class HrMenagerTest {
- private HrMenager hrMenager;
- @BeforeEach
- void setup(){
-	 hrMenager =new HrMenager();
- }
+	private HrMenager hrMenager;
+
+	@BeforeEach
+	void setup() {
+		hrMenager = new HrMenager();
+	}
 
 	@DisplayName("should create non-null employee when given first name, surname and date of birth")
 	@Test
-	void checkNewUser(){
+	void checkNewUser() {
 		//given
-		String firstName="Jacek";
-		String lastName="Plackowski";
-		String dateOfBirth="14-10-1950";
+		String firstName = "Jacek";
+		String lastName = "Plackowski";
+		String dateOfBirth = "14-10-1950";
 
 		//when
-		Employee employee =  hrMenager.create(firstName, lastName, dateOfBirth);
+		Employee employee = hrMenager.create(firstName, lastName, dateOfBirth);
 
 		//then
 		assertThat(employee).isNotNull();
@@ -33,14 +34,14 @@ class HrMenagerTest {
 
 	@DisplayName("should create new employee with all fields: first name, surname and date of birth")
 	@Test
-	void checkUser(){
+	void checkUser() {
 		//given
-		String firstName="Jacek";
-		String lastName="Plackowski";
-		String dateOfBirth="14-10-1950";
+		String firstName = "Jacek";
+		String lastName = "Plackowski";
+		String dateOfBirth = "14-10-1950";
 
 		//when
-		Employee employee =  hrMenager.create(firstName, lastName, dateOfBirth);
+		Employee employee = hrMenager.create(firstName, lastName, dateOfBirth);
 
 		//then
 		assertThat(employee.getFirstName()).isEqualTo(firstName);
@@ -49,14 +50,15 @@ class HrMenagerTest {
 
 
 	}
+
 	@DisplayName("should check if new Employee is added to the database")
 	@Test
-	void saveUser(){
+	void saveUser() {
 		//given
-		String firstName="Jacek";
-		String lastName="Plackowski";
-		String dateOfBirth="14-10-1950";
-		Employee newEmployee =  hrMenager.create(firstName, lastName, dateOfBirth);
+		String firstName = "Jacek";
+		String lastName = "Plackowski";
+		String dateOfBirth = "14-10-1950";
+		Employee newEmployee = hrMenager.create(firstName, lastName, dateOfBirth);
 		//when
 		List<Employee> allEmployees = hrMenager.findAll();
 
@@ -64,42 +66,45 @@ class HrMenagerTest {
 		assertThat(allEmployees).containsOnly(newEmployee);
 
 	}
+
 	@DisplayName("should check database contains all employees")
 	@Test
-	void findUser(){
+	void findUser() {
 		//given
-		Employee adam =  hrMenager.create("Jacek", "Plackowski", "14-10-1950");
-		Employee gorge =  hrMenager.create("Gorge", "Nowak", "17-04-1933");
+		Employee adam = hrMenager.create("Jacek", "Plackowski", "14-10-1950");
+		Employee gorge = hrMenager.create("Gorge", "Nowak", "17-04-1933");
 
 		//when
 		List<Employee> allEmployees = hrMenager.findAll();
 
 		//then
-		assertThat(allEmployees).containsOnly(adam,gorge);
+		assertThat(allEmployees).containsOnly(adam, gorge);
 
 	}
+
 	@DisplayName("should find every employees with given last name")
 	@Test
-	void test03(){
+	void test03() {
 		//given
-		Employee adam =  hrMenager.create("Jacek", "Plackowski", "14-10-1950");
-		Employee gorge =  hrMenager.create("Gorge", "Nowak", "17-04-1933");
-		Employee john =  hrMenager.create("John", "Nowak", "11-04-1963");
+		Employee adam = hrMenager.create("Jacek", "Plackowski", "14-10-1950");
+		Employee gorge = hrMenager.create("Gorge", "Nowak", "17-04-1933");
+		Employee john = hrMenager.create("John", "Nowak", "11-04-1963");
 
 		//when
 		List<Employee> foundEmployees = hrMenager.searchByLastName("Nowak");
 
 		//then
-		assertThat(foundEmployees).containsOnly(gorge,john);
+		assertThat(foundEmployees).containsOnly(gorge, john);
 
 	}
+
 	@DisplayName("Should find no employee when there is no employee with given last name")
 	@Test
-	void test04(){
+	void test04() {
 		//given
-		Employee adam =  hrMenager.create("Jacek", "Plackowski", "14-10-1950");
-		Employee gorge =  hrMenager.create("Gorge", "Nowak", "17-04-1933");
-		Employee john =  hrMenager.create("John", "Nowak", "11-04-1963");
+		Employee adam = hrMenager.create("Jacek", "Plackowski", "14-10-1950");
+		Employee gorge = hrMenager.create("Gorge", "Nowak", "17-04-1933");
+		Employee john = hrMenager.create("John", "Nowak", "11-04-1963");
 
 		//when
 		List<Employee> foundEmployees = hrMenager.searchByLastName("Kszmierski");
@@ -108,62 +113,114 @@ class HrMenagerTest {
 		assertThat(foundEmployees).isEmpty();
 
 	}
-	@DisplayName("Should two employee when their first name matches given search phrase" )
+
+	@DisplayName("Should two employee when their first name matches given search phrase")
 	@Test
-	void test05(){
+	void test05() {
 		//given
-		Employee adam =  hrMenager.create("Jacek", "Plackowski", "14-10-1950");
-		Employee gorge =  hrMenager.create("Jacek", "Nowak", "17-04-1933");
-		Employee john =  hrMenager.create("John", "Nowak", "11-04-1963");
+		Employee adam = hrMenager.create("Jacek", "Plackowski", "14-10-1950");
+		Employee gorge = hrMenager.create("Jacek", "Nowak", "17-04-1933");
+		Employee john = hrMenager.create("John", "Nowak", "11-04-1963");
 
 		//when
 		List<Employee> foundEmployees = hrMenager.searchByPhrase("Jacek");
 
 		//then
-		assertThat(foundEmployees).containsOnly(adam,gorge);
+		assertThat(foundEmployees).containsOnly(adam, gorge);
 
 	}
-	@DisplayName("Should two employee when their last name matches given search phrase" )
+
+	@DisplayName("Should two employee when their last name matches given search phrase")
 	@Test
-	void test06(){
+	void test06() {
 		//given
-		Employee adam =  hrMenager.create("Jacek", "Plackowski", "14-10-1950");
-		Employee gorge =  hrMenager.create("Jacek", "Nowak", "17-04-1933");
-		Employee john =  hrMenager.create("John", "Nowak", "11-04-1963");
+		Employee adam = hrMenager.create("Jacek", "Plackowski", "14-10-1950");
+		Employee gorge = hrMenager.create("Jacek", "Nowak", "17-04-1933");
+		Employee john = hrMenager.create("John", "Nowak", "11-04-1963");
 
 		//when
 		List<Employee> foundEmployees = hrMenager.searchByPhrase("Nowak");
 
 		//then
-		assertThat(foundEmployees).containsOnly(john,gorge);
+		assertThat(foundEmployees).containsOnly(john, gorge);
 
-	}@DisplayName("Should two employee when their date of birth matches given search phrase" )
+	}
+
+	@DisplayName("Should two employee when their date of birth matches given search phrase")
 	@Test
-	void test07(){
+	void test07() {
 		//given
-		Employee adam =  hrMenager.create("Jacek", "Plackowski", "11-04-1963");
-		Employee gorge =  hrMenager.create("Jacek", "Nowak", "17-04-1933");
-		Employee john =  hrMenager.create("John", "Nowak", "11-04-1963");
+		Employee adam = hrMenager.create("Jacek", "Plackowski", "11-04-1963");
+		Employee gorge = hrMenager.create("Jacek", "Nowak", "17-04-1933");
+		Employee john = hrMenager.create("John", "Nowak", "11-04-1963");
 
 		//when
 		List<Employee> foundEmployees = hrMenager.searchByPhrase("11-04-1963");
 
 		//then
-		assertThat(foundEmployees).containsOnly(john,adam);
+		assertThat(foundEmployees).containsOnly(john, adam);
 
-	}@DisplayName("Should sort by first name ascending " )
+	}
+
+	@DisplayName("Should sort by first name ascending ")
 	@Test
-	void test08(){
+	void test08() {
 		//given
-		Employee gorge =  hrMenager.create("Gorge", "Nowak", "17-04-1933");
-		Employee adam =  hrMenager.create("Adam", "Plackowski", "11-04-1963");
-		Employee john =  hrMenager.create("John", "Nowak", "11-04-1963");
+		Employee gorge = hrMenager.create("Gorge", "Nowak", "17-04-1933");
+		Employee adam = hrMenager.create("Adam", "Plackowski", "11-04-1963");
+		Employee john = hrMenager.create("John", "Nowak", "11-04-1963");
 
 		//when
 		List<Employee> sortedEmployees = hrMenager.sortByFirstName();
 
 		//then
-		assertThat(sortedEmployees).containsSequence(adam,gorge,john);
+		assertThat(sortedEmployees).containsSequence(adam, gorge, john);
+
+	}
+
+	@DisplayName("Should sort two employees by first name descending ")
+	@Test
+	void test09() {
+		//given
+		Employee gorge = hrMenager.create("Gorge", "Nowak", "17-04-1933");
+		Employee john = hrMenager.create("John", "Nowak", "11-04-1963");
+
+		//when
+		List<Employee> sortedEmployees = hrMenager.sortByFirstNameWithBubble();
+		//then
+		assertThat(sortedEmployees).containsSequence(john, gorge);
+
+	}
+
+	@DisplayName("Should sort tree employees by first name descending ")
+	@Test
+	void test10() {
+		//given
+		Employee adam = hrMenager.create("Adam", "Plackowski", "11-04-1963");
+		Employee john = hrMenager.create("John", "Nowak", "11-04-1963");
+		Employee gorge = hrMenager.create("Gorge", "Nowak", "17-04-1933");
+
+		//when
+		List<Employee> sortedEmployees = hrMenager.sortByFirstNameWithBubble();
+		//then
+		assertThat(sortedEmployees).containsSequence(john, gorge, adam);
+
+	}
+
+	@DisplayName("Should sort all employees by first name descending ")
+	@Test
+	void test11() {
+		//given
+		Employee adam = hrMenager.create("Adam", "Plackowski", "11-04-1963");
+		Employee gorge = hrMenager.create("Gorge", "Nowak", "17-04-1933");
+		Employee john = hrMenager.create("John", "Nowak", "11-04-1963");
+		Employee make = hrMenager.create("Make", "Nowak", "17-04-1933");
+
+
+		//when
+		List<Employee> sortedEmployees = hrMenager.sortByFirstNameWithBubble();
+		//then
+		assertThat(sortedEmployees).containsSequence(make, john, gorge, adam);
 
 	}
 
