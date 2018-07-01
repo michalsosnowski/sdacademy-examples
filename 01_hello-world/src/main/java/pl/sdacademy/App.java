233f2023a1f92f8acf048bdeb39ad8c3d93733b1
michalsosnowski <mihv.sosna@gmail.com>
2018-07-01 11:23:52 +0200

    Delete unusing staff from 01-helloworld

--- 01_hello-world/src/main/java/pl/sdacademy/App.java
+++ 01_hello-world/src/main/java/pl/sdacademy/App.java
@@ -1,7 +1,5 @@
 package pl.sdacademy;
 
-import java.util.Iterator;
-import java.util.TreeSet;
 
 /**
  * Hello world!
@@ -9,15 +7,6 @@ import java.util.TreeSet;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		TreeSet set = new TreeSet();
-		set.add("one");
-		set.add("two");
-		set.add("three");
-		set.add("four");
-		set.add("one");
 
-		Iterator it = set.iterator();
-		while (it.hasNext()){
-			System.out.println(it.next()+" ");}
 	}
 }
